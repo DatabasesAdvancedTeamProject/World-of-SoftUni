5cfbf2ea9892a22bdb096b54efb7887df1dbcc6e
borko9696 <borko9696@mail.bg>
2016-12-03 01:21:12 +0200

    Create log form Create User Model and hash algorithm for password Log in functionality half done (need to make bug tests)

---
+++ WorldOfSoftUniGAME/Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    using System.Security.Cryptography;
+
+    using WorldOfSoftUniRPG.Data;
+    using WorldOfSoftUniRPG.Models;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorldOfSoftUniContext context = new WorldOfSoftUniContext();
+            User user = context.Users.FirstOrDefault(u => u.UserName == "bobi");
+            Console.WriteLine(user.PasswordHash);
+        }
+    }
+}
